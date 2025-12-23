b690cd7d8e0f39e6f887d3bc6b30e251bc56772f
DhanushAnandAutomation <dhanushanand@outlook.com>
2025-12-23 22:06:16 +0530

    updated

--- src/main/java/SeleniumAdvanced/PageObjects/LoginPage.java
+++ src/main/java/SeleniumAdvanced/PageObjects/LoginPage.java
@@ -1,7 +1,54 @@
 package SeleniumAdvanced.PageObjects;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage  {
 
 	//test
+
+
+WebDriver driver;
+
+	//this is constructor used for initializing the driver from BaseTest
+	public LoginPage(WebDriver driver)
+	{
+		
+		//here, this refers to this class's driver object
+		this.driver=driver;	
+		PageFactory.initElements(driver, this); // this code initialize all the elements. Since constructor runs first, this line also runs with it and thus all WebElements gets initialized first
+	
+	}
+	
+	//normal way of object creation for locator
+	//WebElement userEmail = driver.findElement(By.id("userEmail"));
+	
+	//PageFactory method of object creation for locator
+	@FindBy(id="userEmail")
+	WebElement userEmail;
+	
+	@FindBy(id="userPassword")
+	WebElement userPassword;
+	
+	@FindBy(id="login")
+	WebElement login;
+	
+	
+
+	//methods
+	
+	public void loginApplication(String Email, String Password) {
+		
+		userEmail.sendKeys(Email);
+		userPassword.sendKeys(Password);
+		login.click();
+	}
+
+
+	
+	
 	
 }
