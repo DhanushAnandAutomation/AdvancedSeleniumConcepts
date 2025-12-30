51971ff96a42d40ce19b0af73fb28cc3e5df345b
DhanushAnandAutomation <dhanushanand@outlook.com>
2025-12-31 01:16:17 +0530

    added screnshots

---
+++ src/test/java/seleniumAdvancedConcepts/TestComponents/Listeners.java
@@ -0,0 +1,91 @@
+package seleniumAdvancedConcepts.TestComponents;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import testResources.ExtendReportNG;
+
+public class Listeners extends BaseTest implements ITestListener {
+	
+	ExtentTest test;
+	
+	
+	ExtentReports extent=ExtendReportNG.getReportObject();
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+		test=extent.createTest(result.getMethod().getMethodName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.PASS, "Verified OK"); //this will display when test is success
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.FAIL, "Verified NG");
+		test.fail(result.getThrowable());  //this will catch the actual reason why test failed which is displayed in console
+		
+		try {
+			driver= (WebDriver) result.getTestClass().getRealClass().getField("driver").get(result.getInstance());
+		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			String filePath=  getScreenshot(result.getMethod().getMethodName(), driver);
+			test.addScreenCaptureFromPath(filePath, result.getMethod().getMethodName());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			
+		}
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+		extent.flush();
+		
+	}
+	
+	
+
+}
